1d0552c3939b5e52cfe19077dd87718b0971940c
Carsonmilesfan <carsonaholmes@gmail.com>
2024-11-15 10:51:10 -0800

    finished

---
+++ assignment2.py
@@ -0,0 +1,13 @@
+"""
+
+(2 points) Create a new assignment file called "assignment2.py".
+
+Sample output Task 2
+
+"""
+
+print('I ate some "Frosted Flakes" yesterday morning')
+
+print("or")
+
+print("I ate some \"Frosted Flakes\" yesterday morning")
